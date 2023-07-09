544625a79786fd5d8d3a539277f16958e95f1c97
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-07-10 01:25:41 +0300

    jwt config ayarları yapıldı ve db tarafı olusturuldu

--- ASP.NET_WEBAPI_WITH_IDENTITY_JWT_CHECKING/Program.cs
+++ ASP.NET_WEBAPI_WITH_IDENTITY_JWT_CHECKING/Program.cs
@@ -1,8 +1,50 @@
+using ASP.NET_WEBAPI_WITH_IDENTITY_JWT_CHECKING.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
+//for entity framework
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("connMSSQL")));
+
+//for identity
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+//adding authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWT:ValidAudience"],
+            ValidIssuer = configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+
+        };
+    });
+
+
 builder.Services.AddControllers();
+
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,5 +55,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
